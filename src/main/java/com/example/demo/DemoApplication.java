c4d966b33c7fcf7bda4b9d92c8d62063f8130040
Gary Russell <grussell@pivotal.io>
2019-03-19 11:01:43 -0400

    Add Lazy @RabbitListener

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -15,6 +16,7 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.messaging.handler.annotation.Payload;
 
 @SpringBootApplication
@@ -53,7 +55,6 @@ class StreamConfiguration {
 
 @ConditionalOnClass(Queue.class)
 @ConditionalOnMissingClass("org.springframework.cloud.stream.annotation.EnableBinding")
-@RabbitListener(queues = "foo")
 @Configuration
 class RabbitConfiguration {
 
@@ -61,6 +62,22 @@ class RabbitConfiguration {
 	public Queue fooQueue() {
 		return new Queue("foo");
 	}
+
+	@Bean
+	@Lazy
+	public RabbitConsumer rabbitListener() {
+		return new RabbitConsumer();
+	}
+
+	@Bean
+	public LazyListenerStarter starter(CachingConnectionFactory cf) {
+		return new LazyListenerStarter(cf, "foo", "rabbitListener");
+	}
+
+}
+
+@RabbitListener(queues = "foo")
+class RabbitConsumer {
 
 	@RabbitHandler
 	public void process(@Payload String foo) {
--- src/test/java/com/example/lazy/LazyApplication.java
+++ src/test/java/com/example/lazy/LazyApplication.java
@@ -1,27 +1,14 @@
 package com.example.lazy;
 
-import java.io.IOException;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeoutException;
-
-import com.rabbitmq.client.AMQP.BasicProperties;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.DefaultConsumer;
-import com.rabbitmq.client.Envelope;
-import com.rabbitmq.client.GetResponse;
-
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.connection.Connection;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+
+import com.example.demo.LazyListenerStarter;
 
 @SpringBootApplication
 public class LazyApplication {
@@ -31,7 +18,7 @@ public class LazyApplication {
 	}
 
 	@Bean
-	public LazyListenerStarter starter(ConnectionFactory cf) {
+	public LazyListenerStarter starter(CachingConnectionFactory cf) {
 		return new LazyListenerStarter(cf, "foo", "listener");
 	}
 
@@ -44,74 +31,6 @@ class Listener {
 	@RabbitListener(queues = "foo")
 	public void listen(String in) {
 		System.out.println(in);
-	}
-
-}
-
-class LazyListenerStarter implements SmartInitializingSingleton, ApplicationContextAware {
-
-	private final ConnectionFactory cf;
-
-	private final String queue;
-
-	private final String beanName;
-
-	private ApplicationContext applicationContext;
-
-	LazyListenerStarter(ConnectionFactory cf, String queue, String beanName) {
-		this.cf = cf;
-		this.queue = queue;
-		this.beanName = beanName;
-	}
-
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext)
-			throws BeansException {
-		this.applicationContext = applicationContext;
-	}
-
-	@Override
-	public void afterSingletonsInstantiated() {
-		Executors.newSingleThreadExecutor().execute(() -> {
-			try {
-				Connection conn = this.cf.createConnection();
-				Channel channel = conn.createChannel(false);
-				GetResponse got = channel.basicGet(this.queue, false);
-				if (got != null) {
-					startIt(conn, channel, got.getEnvelope().getDeliveryTag());
-				}
-				else {
-					channel.basicConsume(this.queue, new DefaultConsumer(channel) {
-
-						@Override
-						public void handleDelivery(String consumerTag, Envelope envelope,
-								BasicProperties properties, byte[] body)
-								throws IOException {
-
-							try {
-								getChannel().basicCancel(getConsumerTag());
-								startIt(conn, getChannel(), envelope.getDeliveryTag());
-							}
-							catch (TimeoutException e) {
-								e.printStackTrace();
-							}
-						}
-					});
-				}
-			}
-			catch (Exception e) {
-				e.printStackTrace();
-			}
-		});
-	}
-
-	private void startIt(Connection conn, Channel channel, long deliveryTag)
-			throws IOException, TimeoutException {
-		channel.basicReject(deliveryTag, true);
-		channel.close();
-		conn.close();
-		this.applicationContext.getBean(this.beanName); // DEADLOCK here
-		System.out.println("Started");
 	}
 
 }
