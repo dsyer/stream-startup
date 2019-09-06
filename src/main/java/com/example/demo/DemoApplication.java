4e5e6aae759c6c3f70e488e2e1945b548b71d2a7
Dave Syer <dsyer@pivotal.io>
2019-09-06 15:15:03 +0100

    Fix consumer to always start from beginning

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,8 @@ package com.example.demo;
 import java.util.function.Consumer;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 
 @SpringBootApplication
@@ -17,12 +13,6 @@ public class DemoApplication {
 	public static void main(String[] args) throws Exception {
 		new SpringApplicationBuilder(DemoApplication.class).run(args);
 	}
-}
-
-@ConditionalOnClass(EnableBinding.class)
-@ConditionalOnProperty(prefix = "spring.cloud.stream", name = "enabled", havingValue = "true", matchIfMissing = true)
-@Configuration
-class StreamConfiguration {
 
 	@Bean
 	public Consumer<Message<?>> consumer() {
