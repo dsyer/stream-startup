6ddb61516a5a6053e48b8c2671c56396a7d6f451
Dave Syer <dsyer@pivotal.io>
2019-09-10 10:09:41 +0100

    Hack a bit - ack previous message if offset is low
    You could simulate an effectively once listener this way.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,10 +5,13 @@ import java.util.function.Consumer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.Message;
 
 @SpringBootApplication
 public class DemoApplication {
+
+	private AcknowledgmentOffset ackoff;
 
 	public static void main(String[] args) throws Exception {
 		new SpringApplicationBuilder(DemoApplication.class).run(args);
@@ -18,6 +21,38 @@ public class DemoApplication {
 	public Consumer<Message<?>> consumer() {
 		return message -> {
 			System.err.println(message);
+			Long offset = message.getHeaders().get("kafka_offset", Long.class);
+			if (offset != null) {
+				Acknowledgment ack;
+				if (ackoff != null && ackoff.offset < offset) {
+					if (ackoff.offset < 2) {
+						ack = ackoff.ack;
+						System.err.println("Acking: " + offset);
+						ack.acknowledge();
+					}
+				}
+				ack = message.getHeaders().get("kafka_acknowledgment",
+						Acknowledgment.class);
+				if (ack != null) {
+					System.err.println("Saving: " + offset + ", previous: " + ackoff);
+					ackoff = new AcknowledgmentOffset(ack, offset);
+				}
+			}
 		};
+	}
+
+	static class AcknowledgmentOffset {
+		private Acknowledgment ack;
+		long offset;
+
+		AcknowledgmentOffset(Acknowledgment ack, long offset) {
+			this.ack = ack;
+			this.offset = offset;
+		}
+
+		@Override
+		public String toString() {
+			return "" + offset;
+		}
 	}
 }
