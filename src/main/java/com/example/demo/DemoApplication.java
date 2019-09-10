bd071dfaeda07b80df76ac8c67a2f722201a65be
Dave Syer <dsyer@pivotal.io>
2019-09-10 10:12:27 +0100

    Do the same thing with a ConsumerAwareRebalanceListener

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,17 +1,20 @@
 package com.example.demo;
 
+import java.util.Collection;
 import java.util.function.Consumer;
+
+import org.apache.kafka.common.TopicPartition;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.stream.config.ListenerContainerCustomizer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.listener.AbstractMessageListenerContainer;
+import org.springframework.kafka.listener.ConsumerAwareRebalanceListener;
 import org.springframework.messaging.Message;
 
 @SpringBootApplication
 public class DemoApplication {
-
-	private AcknowledgmentOffset ackoff;
 
 	public static void main(String[] args) throws Exception {
 		new SpringApplicationBuilder(DemoApplication.class).run(args);
@@ -21,38 +24,31 @@ public class DemoApplication {
 	public Consumer<Message<?>> consumer() {
 		return message -> {
 			System.err.println(message);
-			Long offset = message.getHeaders().get("kafka_offset", Long.class);
-			if (offset != null) {
-				Acknowledgment ack;
-				if (ackoff != null && ackoff.offset < offset) {
-					if (ackoff.offset < 2) {
-						ack = ackoff.ack;
-						System.err.println("Acking: " + offset);
-						ack.acknowledge();
-					}
-				}
-				ack = message.getHeaders().get("kafka_acknowledgment",
-						Acknowledgment.class);
-				if (ack != null) {
-					System.err.println("Saving: " + offset + ", previous: " + ackoff);
-					ackoff = new AcknowledgmentOffset(ack, offset);
-				}
+		};
+	}
+
+	@Bean
+	public ListenerContainerCustomizer<AbstractMessageListenerContainer<?, ?>> listenerCustomizer() {
+		return new ListenerContainerCustomizer<AbstractMessageListenerContainer<?, ?>>() {
+
+			@Override
+			public void configure(AbstractMessageListenerContainer<?, ?> container,
+					String destinationName, String group) {
+				System.err.println("Customizing: " + group);
+				container.getContainerProperties().setConsumerRebalanceListener(
+						new ConsumerAwareRebalanceListener() {
+							@Override
+							public void onPartitionsAssigned(
+									org.apache.kafka.clients.consumer.Consumer<?, ?> consumer,
+									Collection<TopicPartition> partitions) {
+								for (TopicPartition partition : partitions) {
+									System.err.println("Seeking: " + partition);
+									consumer.seek(partition, 3);
+								}
+							}
+						});
 			}
 		};
 	}
 
-	static class AcknowledgmentOffset {
-		private Acknowledgment ack;
-		long offset;
-
-		AcknowledgmentOffset(Acknowledgment ack, long offset) {
-			this.ack = ack;
-			this.offset = offset;
-		}
-
-		@Override
-		public String toString() {
-			return "" + offset;
-		}
-	}
 }
